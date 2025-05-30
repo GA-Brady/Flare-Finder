1fe37fe460dda20d40bce5736f65177aaf38afaa
Garrett Brady <gab5654@psu.edu>
2025-05-30 13:09:29 -0400

    Minimal Working hubble_fetch.jl for resolving coordinates of a target

--- src/hubble_fetch.jl
+++ src/hubble_fetch.jl
@@ -2,17 +2,33 @@ using HTTP
 using JSON3
 using DataFrames
 using CSV
+using Tables
+using DataFrames
 
-function mast_query(query::Dict)
-    #= 
-    Preforms the MAST query
+global const MAST_BASE_URL = "https://mast.stsci.edu/api/v0"
+global const CAOM_SEARCH_URL = "$MAST_BASE_URL/invoke"
 
-    Parameters
-    ---------
-    in  => dictionary request (JSON3 object)
-    out <= returns head, content
-    =#
+function mast_query(request::Dict)
+    headers = Dict("Content-type" => "application/x-www-form-urlencoded",
+                "Accept" => "text/plain")
+    body = "request=" * JSON3.write(request)
+    resp = HTTP.post(CAOM_SEARCH_URL, headers, body)
+    return resp
+end
 
-    return head, content
+function main()
+    println("Querying MAST for COS observations of M-dwarf stars...")
+    test_mdwarf = "GJ 176"
+
+    params = Dict("input" => test_mdwarf, "format"=>"json")
+    resolver_request = Dict("service" => "Mast.Name.Lookup",
+    "params" => params)
+
+    print(mast_query(resolver_request))
+end
+
+# Execute the search
+if abspath(PROGRAM_FILE) == @__FILE__
+    results = main()
 end
 
---
+++ src/test.jl
@@ -0,0 +1,4 @@
+function hello_world()
+    print("Hello World!")
+    # execute in terminal using julia --project=. -e "include(\"src/test.jl\"); hello_world()"
+end
